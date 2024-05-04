e33e2f170ecb06cd2d52345a73211cf8ffe17d7d
lddurbin <l.d.durbin@gmail.com>
2024-05-04 16:43:55 +1200

    Long-ago changes

--- tidytuesday/2022/2022-02-01/dogs/app.R
+++ tidytuesday/2022/2022-02-01/dogs/app.R
@@ -7,7 +7,6 @@ library(stringr) # Simple, Consistent Wrappers for Common String Operations
 library(tibble) # Simple Data Frames
 library(showtext) # Using Fonts More Easily in R Graphs 
 
-
 # Data Prep ---------------------------------------------------------------
 showtext_auto()
 walk2(c("Press Start 2P", "Akaya Telivigala"), c("stats", "annotation"), font_add_google)
@@ -25,7 +24,7 @@ traits <- dogs[[2]] %>%
   pivot_longer(-c(breed, dog_id, coat_type, coat_length), names_to = "trait", values_to = "trait_value") %>% 
   mutate(trait = str_replace_all(trait, "_", " "), breed = str_squish(breed)) %>% 
   right_join(dogs[[1]][c(1,12)], by = "dog_id") %>% 
-  mutate(breed = str_sub(breed, end = -2), coat_desc = paste0("Coat is ", str_to_lower(paste(coat_length, coat_type, sep = " \nand "))))
+  mutate(coat_desc = paste0("Coat is ", str_to_lower(paste(coat_length, coat_type, sep = " \nand "))))
 
 
 # Shiny UI ----------------------------------------------------------------
@@ -45,13 +44,13 @@ server <- function(input, output) {
   output$plot <- renderPlot({
     selected_dog <- traits %>% filter(breed == input$dog)
     
-    ggplot(tibble::tibble(x = c(0,10), y = c(-9,16))) +
-      theme_void(base_family = "google") +
+    ggplot(tibble(x = c(0,10), y = c(-9,16))) +
+      theme_void() +
       ggimage::geom_image(aes(x = 5, y = 10, image = selected_dog$image[1]), size = 0.35) +
-      geom_text(aes(x = 5, y = 16, label = selected_dog$breed[1], family = "stats"), size = 7.5) +
+      geom_text(aes(x = 5, y = 16, label = selected_dog$breed[1]), size = 7.5, family = "stats") +
       annotate(geom = "curve", x = 7, y = 13, xend = 6, yend = 12, curvature = 0.2, arrow = arrow(length = unit(2, "mm")), size = 0.75) +
       annotate(geom = "text", x = 7.2, y = 13, label = selected_dog$coat_desc[1], hjust = "left", size = 6, family = "annotation") +
-      pmap(list(rep(1, 14), seq(-8.5, 4.5, 1), traits %>% distinct(trait) %>% pull()), ~geom_text(aes(x = ..1, y = ..2, family = "stats"), label = str_to_sentence(..3), size = 5, hjust = 0)) +
+      pmap(list(rep(1, 14), seq(-8.5, 4.5, 1), traits %>% distinct(trait) %>% pull()), ~geom_text(aes(x = ..1, y = ..2), label = str_to_sentence(..3), size = 5, family = "stats", hjust = 0)) +
       pmap(list(rep(9, 14), seq(-8.5, 4.5, 1), traits %>% distinct(trait) %>% pull()), ~geom_text(aes(x = ..1, y = ..2, label = selected_dog$trait_value[selected_dog$trait==..3], family = "stats"), size = 5)) +
       scale_y_continuous(limits = c(-9,16)) +
       scale_x_continuous(limits = c(0,10))
