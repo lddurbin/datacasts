6771a369acd2b27ad8141e5ffb5d2270e034792c
lddurbin <l.d.durbin@gmail.com>
2024-05-06 19:13:36 +1200

    Code used in video

--- convert_heic.R
+++ convert_heic.R
@@ -3,8 +3,10 @@ library(magick)
 reformat_image <- function(filepath, format = c("png", "jpg"), size = "800x") {
   format <- match.arg(format)
   
+  filename <- basename(filepath)
+  filename_no_ext <- substr(filename, 1, nchar(filename)-4)
+  
   image_read(filepath) |> 
-    image_convert(format = format) |> 
     image_resize(size) |> 
-    image_write(paste0(basename(filepath), ".", format))
+    image_write(paste0(filename_no_ext, format))
 }
